14a32c84c1402bb4817a477c2aabf1a5b562954f
Yasmojam <yazzij97@gmail.com>
2018-10-13 14:57:47 +0100

    turret

--- src/babybot/utils.py
+++ src/babybot/utils.py
@@ -8,12 +8,23 @@ def heading_from_to(p1: Vector, p2: Vector) -> float:
     """
     Returns the heading in degrees from point 1 to point 2
     """
-    pass
+    x1 = p1[0]
+    y1 = p1[1]
 
+    x2 = p2[0]
+    y2 = p2[1]
+
+    angle = atan2(y2-y1, x2-x1) * (180/pi)
+    angle = (angle - 360) % 360
+    return abs(angle)
 
 def should_turn_left(current_heading: float, goal_heading: float) -> bool:
     """
     Returns true if the tank should turn left to aim at the goal
     heading.
     """
-    pass
+    diff = goal_heading - current_heading
+    if diff > 0:
+        return diff > 180
+    else:
+        return diff >= -180
