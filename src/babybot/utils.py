2d8b6c1c249f69f0d2420de4e4399d108d251af0
pmaitland <pmaitland98@gmail.com>
2018-10-13 14:55:58 +0100

    implemented perform method for gotogoalstate

--- src/babybot/utils.py
+++ src/babybot/utils.py
@@ -17,3 +17,28 @@ def should_turn_left(current_heading: float, goal_heading: float) -> bool:
     heading.
     """
     pass
+
+def closest_point(current_point, points) -> Vector:
+    """
+    Returns the point from a list of points which is closest to the given
+    point.
+    """
+    closest = None
+    distance_to_closest = math.inf
+
+    for other_point in points:
+        distance = calculate_distance(current_point, other_point)
+        if (distance < distance_to_closest):
+            closest = other_point
+            distance_to_closest = distance
+
+    return other_point
+
+def calculate_distance(point1, point2) -> float:
+    """
+    Returns the distances between two points as a float
+    """
+    x = point2[0] - point1[0]
+    y = point2[1] - point1[1]
+
+    return (math.sqrt(x*x + y*y))
