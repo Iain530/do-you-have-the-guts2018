5adc32947ab38f633b0f5bf13565cabd810a861f
Iain Cattermole <iain_c@live.co.uk>
2018-10-13 12:48:45 +0100

    status

--- src/babybot/statemachine.py
+++ src/babybot/statemachine.py
@@ -10,11 +10,11 @@ class StateMachine:
     def __init__(self, GameServer, name) -> None:
         self.status = Status(name=name)
         self.GameServer = GameServer
-        self.turretcontrols = TurretMovement(GameServer=GameServer, status=status)
-        self.bodycontrols = BodyMovement(GameServer=GameServer, status=status)
+        self.turretcontrols = TurretMovement(GameServer=GameServer, status=self.status)
+        self.bodycontrols = BodyMovement(GameServer=GameServer, status=self.status)
         self.current_state = STATES[0]
 
-    def update(self):
+    def update(self, message: Message):
         pass
 
     def choose_state(self) -> None:
